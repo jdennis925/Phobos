c165a9c2aaf25212e60eabe634afdf5746e0453b
John <jdennis925@gmail.com>
2016-05-18 18:07:37 -0500

    dynamic board size
    Dummy tile data

--- public/js/app.js
+++ public/js/app.js
@@ -11,10 +11,34 @@ angular.module('rpgmapapp', ['routerRoutes'])
     
     .controller('aboutController', function () {
         var vm = this;
-        vm.message = "RPG Map App is created for virtual tabletop combat.";
+        vm.message = "RPG Map App virtual tabletop combat.";
     })
 
     .controller('boardController', function () {
         var vm = this;
         vm.boardName = "The Board Name";
+        
+         vm.RowCount = {};
+         vm.ColCount = {};
+        
+        vm.boardArray = [{
+                'tiles': [{
+                    'color': 'green',
+                }]
+        }];
+        
+        vm.CreateBoardArray = function () {
+            vm.boardArray = [];
+            
+            for (var rowIndex = 0; rowIndex < vm.RowCount.value; rowIndex++) {
+                var row = [];
+                for (var colIndex = 0; colIndex < vm.ColCount.value; colIndex++) {
+                    
+                    row.push({
+                        'color' : 'blue' + rowIndex + colIndex,
+                    })
+                }
+                vm.boardArray.push({'tiles' : row});
+            }
+        }
     })
